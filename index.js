36d11b459e5859fa0195200ec4bc11091c1d0317
kennylongsheng <kennylongsheng@gmail.com>
2019-01-19 12:01:08 +0800

    consoleLog Body

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.listen(process.env.PORT || 9482 ,() => console.log('webhook is listening'));
 app.post('/webhook', (req,res) => {
 	let body = req.body;
 	
+	console.log(body)
+
 	// Check this is an event from a page subscription
 	if (body.object === 'page'){
 		// Iterates over each entry - there may be multiple if batched
