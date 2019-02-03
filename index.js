e56d5e564d1137a0b213ea97c0167f95ced53812
kennylongsheng <kennylongsheng@gmail.com>
2019-02-03 17:00:50 +0800

    Try DB

--- index.js
+++ index.js
@@ -91,6 +91,8 @@ let distinguishMSG = function(Sender_ID, Message_Input){
 	// number <Name>
 	if(Message_Input.includes("number")){ 
 		let queryNameString = {name : { $regex: / + Message_Array[Message_Array.indexOf("number") + 1] + /i} };
+		console.log(queryNameString);
+		console.log(typeof(queryNameString));
 		let queryName = JSON.parse(queryNameString); 
 		queryDB(Sender_ID, queryName, sendAPI);
 		Query_Type_Correct = false;
