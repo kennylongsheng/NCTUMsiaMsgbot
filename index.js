65a82f61ac006edfba2188ccc5ea8cd83d731287
kennylongsheng <kennylongsheng@gmail.com>
2019-02-03 23:39:33 +0800

    Try DB

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ let queryDB = function(qname, Sender_ID, send){
 		cursor.forEach(function(doc){
 			let message = counter+".) "+doc.course+" "+doc.year+" "+doc.name+" "+doc.phoneno+"\n";
 			messageParser[counter] = message;
-			msgPar.push(msgArray)
+			msgPar.push(message)
 			counter += 1;
 			//console.log(JSON.stringify(doc));
 		},
