2d70093d5a650d8b02cee87601240a6abeacfa36
kennylongsheng <kennylongsheng@gmail.com>
2019-02-03 20:41:23 +0800

    Try DB

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ let queryDB = function(qname, Sender_ID, send){
 
 		const db = client.db("nctumycommunity");
 		let cursor = db.collection('info').find(qname).sort({course: 1, year: 1}); // "{ $regex: /" +qname+"/ }"
-		let messageParser = [];
+		let messageParser;
 		// cursor.forEach(function(doc){
 		// 	let message = counter+".) "+doc.course+" "+doc.year+" "+doc.name+" "+doc.phoneno+"\n";
 		// 	counter += 1;
