8f7ec00e249a5e51f0d531c3405a1432e968ece7
kennylongsheng <kennylongsheng@gmail.com>
2019-02-06 13:05:44 +0800

    Try DB

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const KennyPSID = process.env.KENNYPSID;
 const MlabURI = process.env.MLABURI;
 const HELP_PTR = fs.readFileSync('txt/help.txt','utf8');
-const msgPar = new queue();
+var msgPar = new queue();
 
 //////////////////////////////////SETUP WEBHOOK--Don't Change//////////////////////////////////////////////////
 app.listen(process.env.PORT || 9482 ,() => console.log('webhook is listening'));
